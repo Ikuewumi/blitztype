f5e1126521c80a7ee61f87f60e094932050849fc
Ikuewumi <ayobamsik@gmail.com>
2024-03-25 20:31:32 +0100

    @feat - Add Github Actions, among other things

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
-
+import '@scss/index.scss'
 import App from './App'
 
 const root = document.getElementById('root')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
-
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid'
+import path from "path";
 
 export default defineConfig({
   plugins: [solid()],
@@ -15,7 +15,16 @@ export default defineConfig({
     setupFiles: './setupVitest.ts',
   },
   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@composables': path.resolve(__dirname, './src/composables'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@scss': path.resolve(__dirname, './src/scss')
+    },
     conditions: ['development', 'browser'],
-  },
+  }
+
 
 })
